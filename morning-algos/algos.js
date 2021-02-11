c45e7707c39a26a5b2837f79f40a186a276e4b14
Michael Shin <michaelshin@Admins-MacBook-Pro.local>
2021-02-11 10:19:49 -0800

    adding delete method && size method

--- morning-algos/algos.js
+++ morning-algos/algos.js
@@ -68,8 +68,60 @@ class SLL {
         newNode.next = this.head // set the new node's next to the head
         this.head = newNode // move the head to the new node
     }
+    size() {
+        let count = 0;
+        let runner = this.head;
+        while (runner) {
+            count++;
+            runner = runner.next
+        }
+        return count;
+    }
+    delete(data) {
+        // if val is contained within the current list, delete it.
+        // return void
+        // assume there are no duplicates
+        // consider the edge case if you have to delete the head node
+        // consider the edge case your list is empty
+        // consider the edge case that your list does not contain the val
 
+        let runner = this.head
+        // check if head is empty
+        if (!runner) {
+            return;
+        }
+        // check if head is target 
+        if (runner.data == data) {
+            this.removeFromFront()
+            return
+        }
+        // while next exists
+        while (runner.next) {
+            // check if runner next is data
+            if (runner.next.data == data) {
+                // remove it and return 
+                runner.next = runner.next.next
+                return
+            }
+        }
+        // other traverse
+        runner = runner.next
+
+    }
+    deleteV2(data) {
+        var runner = this.head
+        var prev = null
+
+        // if head is val 
+
+    }
+
+    clear() {
+        this.head = null
+    }
 }
+
+
 
 var mySLL = new SLL()
 mySLL.addDataToFront(10)
@@ -84,6 +136,9 @@ mySLL.removeFromFront()
 mySLL.read()
 console.log(mySLL.contains(100))
 console.log(mySLL.contains(50))
+console.log(mySLL.size())
+// console.log(mySLL.clear())
+console.log(mySLL.delete(100))
 // var node = new Node(10)
 // node.data = 10
 // node.next = new Node()
